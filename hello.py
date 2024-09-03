a88f51c5b92b837903ebba15402b70ac115871ee
lucccasm <lucas.montpetit@gmail.com>
2024-09-03 16:49:54 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,7 @@
+import math
+#TODO Affichez l'aire d'un cercle avec le rayon indiqué
+rayon = 4
+air = math.pi*rayon**2
+print(air)
+#TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Lucas Montpetit")
